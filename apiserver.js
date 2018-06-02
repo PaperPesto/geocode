667cb8d49325ae334f57471fff1a183d3823261a
DESKTOP-KF4SFLC\Leonardo <leonardo.bianchini90@gmail.com>
2018-06-02 18:34:30 +0200

    API Rest ok

--- apiserver.js
+++ apiserver.js
@@ -1,1 +1,23 @@
 console.log('starting apiserver.js');
+
+const express = require('express');
+const geocode = require('./geocode.js');
+
+var app = express();
+
+var address = 'Via argomenna';
+var credentials = 'AIzaSyCiNds_rC3hSNSd2PlV828fCI-VdrYB4RQ';
+
+app.get('/geoGet', (req, res) => {
+    geocode.getAddressInfo(address, credentials, (errorMessage, result) => {
+        console.log(result);
+        res.end(JSON.stringify(result, undefined, 2));    
+    });
+})
+
+var server = app.listen(5000, () => {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Server listening at http://%s:%s", host, port)
+})
