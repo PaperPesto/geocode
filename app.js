58a318d8c2793631f8487112d137f586bd87141d
DESKTOP-KF4SFLC\Leonardo <leonardo.bianchini90@gmail.com>
2018-06-02 18:06:46 +0200

    Implementazione di tutto; funge

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 console.log('Starting app.js');
 
-var request = require('request');
+const yargs = require('yargs');
+const geocode = require('./geocode.js');
+
+const argv = yargs
+.options({
+    address:{
+        demand: true,
+        alias: 'a',
+        describe: 'Address',
+        string: true
+    }
+})
+.help()
+.argv;
+
+var address = argv.address;
+var credentials = 'AIzaSyCiNds_rC3hSNSd2PlV828fCI-VdrYB4RQ';
+
+geocode.getAddressInfo(address, credentials, (errorMessage, result) => {
+    if(errorMessage){
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(result, undefined, 2));
+    }
+});
+
+
--- geocode.js
+++ geocode.js
@@ -1,1 +1,28 @@
 console.log('starting goecode.js');
+
+var request = require('request');
+
+var getAddressInfo = (address, credentials, callback) => {
+
+    var encodedAddress = encodeURIComponent(address);
+    var APIUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${credentials}`;
+
+    request({
+        url: APIUrl,
+        json: true
+    }, (error, response, body) => {
+        if(error) {
+            callback('Unable to connect to google server.');
+        } else if (body.status === 'ZERO_RESULTS') {
+            callback('Unable to find address.');
+        } else if (body.status = 'OK') {
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            });
+        }
+    });
+};
+
+module.exports.getAddressInfo = getAddressInfo;
