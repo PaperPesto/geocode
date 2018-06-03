52b7dbe1f6c06f471b678afa64700ecaf42d5333
DESKTOP-KF4SFLC\Leonardo <leonardo.bianchini90@gmail.com>
2018-06-03 13:05:04 +0200

    Aggiunta API per post

--- apiserver.js
+++ apiserver.js
@@ -2,15 +2,21 @@ console.log('starting apiserver.js');
 
 const express = require('express');
 const geocode = require('./geocode.js');
+const bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
 
-var address = 'Via argomenna';
 var credentials = 'AIzaSyCiNds_rC3hSNSd2PlV828fCI-VdrYB4RQ';
 
-app.get('/geoGet', (req, res) => {
-    geocode.getAddressInfo(address, credentials, (errorMessage, result) => {
-        console.log(result);
+app.get('/', function(req, res) {
+    res.json({ message: 'Raspberry Pi-3 Via verdi API' });   
+});
+
+app.post('/getAddress', (req, res) => {
+    geocode.getAddressInfo(req.body.address, credentials, (errorMessage, result) => {
+        console.log('Request body:', JSON.stringify(req.body));
+        console.log('Response body:', JSON.stringify(result));
         res.end(JSON.stringify(result, undefined, 2));    
     });
 })
@@ -20,4 +26,4 @@ var server = app.listen(5000, () => {
     var port = server.address().port
 
     console.log("Server listening at http://%s:%s", host, port)
-})
+});
